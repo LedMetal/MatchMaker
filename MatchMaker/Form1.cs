72facb7894c57e7ba59764e6d4fc8b6c2973a7f8
Abdul Sadiq <abdulsadiq@hotmail.com>
2018-02-23 02:59:37 -0500

    Write code to get folder path from dialog selection

--- MatchMaker/Form1.cs
+++ MatchMaker/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,24 @@ namespace MatchMaker
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnBrowseInput_Click(object sender, EventArgs e)
+        {
+            // Create FolderBrowserDialog object
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            // Create DialogResult object that reads the user's input from the dialog
+            DialogResult diagResult = folderDialog.ShowDialog();
+
+            if (diagResult == DialogResult.OK)
+            {
+                // Display selected path in read-only textboxes
+                tbSelectedDirectory.Text = folderDialog.SelectedPath;
+                tbOutputDirectory.Text = folderDialog.SelectedPath;
+
+                // Display number of files found
+                tbFilesFound.Text = Directory.GetFiles(folderDialog.SelectedPath).Length.ToString();
+            }
         }
     }
 }
