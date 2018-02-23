b1c00771f678bd3a635d63b48880bf9e3ac1dc1b
Abdul Sadiq <abdulsadiq@hotmail.com>
2018-02-23 02:48:31 -0500

    Create Pairing, SubTrial and Trial business classes

---
+++ MatchMaker/bus/SubTrial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchMaker.bus
+{
+    public class SubTrial
+    {
+        List<Pairing> subTrialPairings;
+        double subTrialAverageSimilarity;
+
+        public SubTrial()
+        {
+            this.subTrialPairings = new List<Pairing>();
+            this.subTrialAverageSimilarity = 0.0d;
+        }
+
+        public SubTrial(List<Pairing> subTrialPairings, double subTrialaverageSimilarity)
+        {
+            this.subTrialPairings = subTrialPairings;
+            this.subTrialAverageSimilarity = subTrialaverageSimilarity;
+        }
+
+        public void CalculateAverageSimilarity()
+        {
+            this.subTrialAverageSimilarity = Convert.ToDouble(this.subTrialPairings.Average(x => x.SimilarityPercent));
+        }
+
+
+        /*---------------------------------------------------------------------------------------------------------*/
+
+        public List<Pairing> SubTrialPairings { get => subTrialPairings; set => subTrialPairings = value; }
+        public double SubTrialAverageSimilarity { get => subTrialAverageSimilarity; set => subTrialAverageSimilarity = value; }
+    }
+}
---
+++ MatchMaker/bus/Trial.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchMaker.bus
+{
+    public class Trial
+    {
+        List<SubTrial> subTrialsList;
+        double trialAverageSimilarity, trialStandardDeviation;
+
+        public Trial()
+        {
+            this.subTrialsList = new List<SubTrial>();
+            this.trialAverageSimilarity = 0.0d;
+            this.trialStandardDeviation = 0.0d;
+        }
+
+        public Trial(List<SubTrial> subTrialsList, double trialAverageSimilarity, double trialStandardDeviation)
+        {
+            this.subTrialsList = subTrialsList;
+            this.trialStandardDeviation = trialAverageSimilarity;
+            this.trialStandardDeviation = trialStandardDeviation;
+        }
+
+        public void CalculateTrialAverageSimilarity()
+        {
+            this.trialAverageSimilarity = this.subTrialsList.Average(x => x.SubTrialAverageSimilarity);
+        }
+
+        public void CalculateTrialStandardDeviation()
+        {
+            int count = this.subTrialsList.Count();
+
+            // Sum of (x - average)^2
+            double sum = this.subTrialsList.Sum(x => (Math.Abs(x.SubTrialAverageSimilarity - this.trialAverageSimilarity)) * (Math.Abs(x.SubTrialAverageSimilarity - this.trialAverageSimilarity)));
+
+            // Standard Deviation equals square root of (Sum divided by count)
+            this.trialStandardDeviation = Math.Sqrt(sum / count);
+        }
+
+
+        /*-------------------------------------------------------------------------------------------------------------*/
+
+        public List<SubTrial> SubTrialsList { get => subTrialsList; set => subTrialsList = value; }
+        public double TrialAverageSimilarity { get => trialAverageSimilarity; set => trialAverageSimilarity = value; }
+        public double TrialStandardDeviation { get => trialStandardDeviation; set => trialStandardDeviation = value; }
+    }
+}
