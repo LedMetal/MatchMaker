2854b0ccaf0325ab6f67432d65c87b59943ab4d2
Abdul Sadiq <abdulsadiq@hotmail.com>
2018-02-23 02:39:00 -0500

    Create look of main form

--- MatchMaker/Form1.Designer.cs
+++ MatchMaker/Form1.Designer.cs
@@ -28,12 +28,242 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tbDisplay = new System.Windows.Forms.TextBox();
+            this.btnRunExperiment = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbFilesFound = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbOutputDirectory = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnBrowseOutput = new System.Windows.Forms.Button();
+            this.tbSelectedDirectory = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnBrowseInput = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbTrials = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(22, 374);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(73, 24);
+            this.label10.TabIndex = 36;
+            this.label10.Text = "Step #4";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(12, 61);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(496, 15);
+            this.label9.TabIndex = 35;
+            this.label9.Text = "Calculating Average Percent Similarities Among Trials Of Randomly Paired Conversa" +
+    "tions";
+            // 
+            // tbDisplay
+            // 
+            this.tbDisplay.Location = new System.Drawing.Point(269, 87);
+            this.tbDisplay.Multiline = true;
+            this.tbDisplay.Name = "tbDisplay";
+            this.tbDisplay.ReadOnly = true;
+            this.tbDisplay.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbDisplay.Size = new System.Drawing.Size(242, 338);
+            this.tbDisplay.TabIndex = 34;
+            // 
+            // btnRunExperiment
+            // 
+            this.btnRunExperiment.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRunExperiment.Location = new System.Drawing.Point(113, 385);
+            this.btnRunExperiment.Name = "btnRunExperiment";
+            this.btnRunExperiment.Size = new System.Drawing.Size(149, 40);
+            this.btnRunExperiment.TabIndex = 33;
+            this.btnRunExperiment.Text = "Run Analysis";
+            this.btnRunExperiment.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(22, 306);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 24);
+            this.label8.TabIndex = 32;
+            this.label8.Text = "Step #3";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(22, 210);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 24);
+            this.label7.TabIndex = 31;
+            this.label7.Text = "Step #2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(22, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 24);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "Step #1";
+            // 
+            // tbFilesFound
+            // 
+            this.tbFilesFound.Location = new System.Drawing.Point(219, 177);
+            this.tbFilesFound.Name = "tbFilesFound";
+            this.tbFilesFound.ReadOnly = true;
+            this.tbFilesFound.Size = new System.Drawing.Size(43, 20);
+            this.tbFilesFound.TabIndex = 29;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(149, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 13);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Files Found:";
+            // 
+            // tbOutputDirectory
+            // 
+            this.tbOutputDirectory.Location = new System.Drawing.Point(26, 267);
+            this.tbOutputDirectory.Name = "tbOutputDirectory";
+            this.tbOutputDirectory.ReadOnly = true;
+            this.tbOutputDirectory.Size = new System.Drawing.Size(236, 20);
+            this.tbOutputDirectory.TabIndex = 27;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(68, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 13);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Select output directory";
+            // 
+            // btnBrowseOutput
+            // 
+            this.btnBrowseOutput.Location = new System.Drawing.Point(187, 238);
+            this.btnBrowseOutput.Name = "btnBrowseOutput";
+            this.btnBrowseOutput.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseOutput.TabIndex = 25;
+            this.btnBrowseOutput.Text = "Browse...";
+            this.btnBrowseOutput.UseVisualStyleBackColor = true;
+            // 
+            // tbSelectedDirectory
+            // 
+            this.tbSelectedDirectory.Location = new System.Drawing.Point(26, 151);
+            this.tbSelectedDirectory.Name = "tbSelectedDirectory";
+            this.tbSelectedDirectory.ReadOnly = true;
+            this.tbSelectedDirectory.Size = new System.Drawing.Size(236, 20);
+            this.tbSelectedDirectory.TabIndex = 24;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Select folder directory of .txt files";
+            // 
+            // btnBrowseInput
+            // 
+            this.btnBrowseInput.Location = new System.Drawing.Point(188, 121);
+            this.btnBrowseInput.Name = "btnBrowseInput";
+            this.btnBrowseInput.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseInput.TabIndex = 22;
+            this.btnBrowseInput.Text = "Browse...";
+            this.btnBrowseInput.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(167, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 36);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "MatchMaker";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 339);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Number of Trials:";
+            // 
+            // tbTrials
+            // 
+            this.tbTrials.Location = new System.Drawing.Point(163, 336);
+            this.tbTrials.Name = "tbTrials";
+            this.tbTrials.Size = new System.Drawing.Size(100, 20);
+            this.tbTrials.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(523, 440);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.tbDisplay);
+            this.Controls.Add(this.btnRunExperiment);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbFilesFound);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbOutputDirectory);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnBrowseOutput);
+            this.Controls.Add(this.tbSelectedDirectory);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnBrowseInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbTrials);
+            this.Name = "Form1";
+            this.Text = "MatchMaker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox tbDisplay;
+        private System.Windows.Forms.Button btnRunExperiment;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbFilesFound;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbOutputDirectory;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnBrowseOutput;
+        private System.Windows.Forms.TextBox tbSelectedDirectory;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnBrowseInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbTrials;
     }
 }
 
